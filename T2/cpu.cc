ae9a9abea92e144e80089896a161a8410458700b
Victor Gouvêa <victorgouvea28@gmail.com>
2022-09-12 15:44:30 -0300

    Add arquivos base T2

---
+++ T2/cpu.cc
@@ -0,0 +1,26 @@
+#include "cpu.h"
+#include <iostream>
+
+__BEGIN_API
+
+void CPU::Context::save()
+{
+    getcontext(&_context);
+}
+
+void CPU::Context::load()
+{
+    setcontext(&_context);
+}
+
+CPU::Context::~Context()
+{
+    delete[] _stack;
+}
+
+void CPU::switch_context(Context *from, Context *to)
+{
+    swapcontext(&(from->_context), &(to->_context));
+}
+
+__END_API
---
+++ T2/cpu.h
@@ -0,0 +1,55 @@
+#ifndef cpu_h
+#define cpu_h
+
+#include <ucontext.h>
+#include <iostream>
+#include "traits.h"
+
+__BEGIN_API
+
+class CPU
+{
+    public:
+
+        class Context
+        {
+        private:
+            static const unsigned int STACK_SIZE = Traits<CPU>::STACK_SIZE;
+        public:
+            Context() { _stack = 0; }
+
+            template<typename ... Tn>
+            Context(void (* func)(Tn ...), Tn ... an);
+
+            ~Context();
+
+            void save();
+            void load();
+
+        private:            
+            char *_stack;
+        public:
+            ucontext_t _context;
+        };
+
+    public:
+        static void switch_context(Context *from, Context *to);
+
+};
+
+template<typename ... Tn>
+CPU::Context::Context(void (* func)(Tn ...), Tn ... an)
+{
+    _stack = new char[STACK_SIZE];
+    getcontext(&_context);
+    _context.uc_link=0;
+    _context.uc_stack.ss_sp=(void *)(_stack);
+    _context.uc_stack.ss_size=STACK_SIZE;
+    _context.uc_stack.ss_flags=0;
+    makecontext(&_context, (void (*)())(func), sizeof...(an), an...);  
+}
+
+__END_API
+
+#endif
+
